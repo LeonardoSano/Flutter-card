fd1d632fc647c2cbc16e252c2db00141b6ba7a1d
leonardosano1995@gmail.com <leonardosano95@gmail.com>
2020-03-24 17:35:48 +0100

    completato

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
@@ -17,16 +17,15 @@ class MyApp extends StatelessWidget {
               children: <Widget>[
                 CircleAvatar(
                   radius: 50.0,
-                  backgroundImage:AssetImage('images/leo.jpg'),
+                  backgroundImage: AssetImage('images/leo.jpg'),
                 ),
                 Text(
                   'Leonardo Sanò',
                   style: TextStyle(
-                    fontSize: 40.0,
-                    fontFamily: 'Pacifico',
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold
-                  ),
+                      fontSize: 40.0,
+                      fontFamily: 'Pacifico',
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
                 ),
                 Text(
                   'FLUTTER DEVELOPER',
@@ -35,51 +34,44 @@ class MyApp extends StatelessWidget {
                       fontFamily: 'SourceSansPro',
                       color: Colors.teal.shade200,
                       letterSpacing: 2.5,
-                      fontWeight: FontWeight.bold
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 180.0,
+                  child: Divider(
+                    color: Colors.teal[100],
                   ),
                 ),
-                Container(
+                Card(
                   color: Colors.white,
-                  padding: EdgeInsets.all(10.0),
-                  margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(Icons.phone,
-                      color: Colors.teal
-                      ),
-                      SizedBox(
-                        width: 20.0,
-                      ),
-                      Text('+39 123456789',
-                        style: TextStyle(
-                          fontSize: 20.0,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.teal),
+                    title: Text(
+                      '+39 123456789',
+                      style: TextStyle(
+                          fontSize: 17.0,
                           fontFamily: 'SourceSansPro',
-                          color: Colors.teal
-                        ),
-                      ),
-                    ],
+                          letterSpacing: 1.5,
+                          color: Colors.teal),
+                    ),
                   ),
                 ),
-                Container(
+                Card(
                   color: Colors.white,
-                  padding: EdgeInsets.all(10.0),
-                  margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(Icons.email,
-                          color: Colors.teal
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text('leonardosano1995@gmail.com',
-                        style: TextStyle(
-                            fontSize: 18.0,
-                            fontFamily: 'SourceSansPro',
-                            color: Colors.teal
-                        ),
-                      ),
-                    ],
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.teal),
+                    title: Text(
+                      'leonardosano1995@gmail.com',
+                      style: TextStyle(
+                          fontSize: 17.0,
+                          fontFamily: 'SourceSansPro',
+                          color: Colors.teal),
+                    ),
                   ),
                 ),
               ],
